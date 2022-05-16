c847e3178352f866e0679b3da737893e89f29c5c
Cesarsp41 <64189944+Cesarsp41@users.noreply.github.com>
2022-05-16 00:22:41 -0500

    Update Practica8.py

--- Practica8.py
+++ Practica8.py
@@ -8,3 +8,21 @@ print('Presiona "espacio" para cerrar el video.')
 while(cam.isOpened()):
     ready,hMirror = cam.read()
     imgOriginal = cv2.flip(hMirror,1)
+    
+    laplacian = cv2.Laplacian(imgOriginal, cv2.CV_64F)
+    Sobelx = cv2.Sobel(imgOriginal, cv2.CV_64F,1,0,ksize=5)
+    Sobely = cv2.Sobel(imgOriginal, cv2.CV_64F,0,1,ksize=5)
+    Sobel = cv2.Sobel(imgOriginal, cv2.CV_64F,1,1,ksize=5)
+    edges = cv2.Canny(imgOriginal, 100, 100)
+    
+    if ready == True:
+        cv2.imshow('Original',imgOriginal)
+        cv2.imshow('Laplacian',laplacian)
+        cv2.imshow('Sobel X',Sobelx)
+        cv2.imshow('Sobel Y',Sobely)
+        cv2.imshow('Sobel',Sobel)
+        cv2.imshow('Edges',edges)
+        if cv2.waitKey(1) & 0xFF == ord(' '):
+            cv2.destroyAllWindows()
+            break
+

c4e81dd7dfb230cb54cc0b1cef49891a181a888b
Cesarsp41 <64189944+Cesarsp41@users.noreply.github.com>
2022-05-16 00:22:18 -0500

    Update Practica8.py

--- Practica8.py
+++ Practica8.py
@@ -4,3 +4,7 @@ import cv2
 cam = cv2.VideoCapture(1)
 
 print('Presiona "espacio" para cerrar el video.')
+
+while(cam.isOpened()):
+    ready,hMirror = cam.read()
+    imgOriginal = cv2.flip(hMirror,1)
